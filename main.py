62263450f6259568a01fcde42e71dab97a533a47
ThewShadow <zvichayniy.vick@gmail.com>
2022-05-20 13:44:39 +0000

    work

--- main.py
+++ main.py
@@ -3,27 +3,38 @@ import telebot
 import flask
 import bitrix24 as b24
 import socket
+import time
 
 local_ip = socket.gethostbyname(socket.gethostname())
 
 token = '5237614948:AAF1sfGVhikBmAb9BeN_2YeQj-n2akZjDBA'
-webhook_url = f'https:{local_ip}/bots/helper/'
+webhook_url = f'https://167.71.78.210:8443/bots/helper/{token}'
 bot = telebot.TeleBot(token)
 
-bot.delete_webhook()
-time.sleep(2)
-bot.set_webhook(webhook_url, certificate=open('webhook.crt', 'r'), max_connections=5)
+
 
 app = flask.Flask(__name__)
 
-@app.route('bots/helper')
+@app.route(f'/bots/helper/{token}', methods=['POST'])
 def webhook_handler():
     json_data = flask.request.get_data().decode('utf-8')
-    bot.process_new_updates(telebot.types.Update.de_json(json_data))
+    bot.process_new_updates([telebot.types.Update.de_json(json_data)])
+    return ''
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    pass	
     bot.send_message(message.chat.id, 'work')
+
+for i in range(3):
+    try:
+        bot.delete_webhook()
+        time.sleep(1.5)
+        bot.set_webhook(webhook_url, certificate=open('webhook.crt', 'r'), max_connections=5)
+        break
+    except:
+        continue
 
 application = app
 
